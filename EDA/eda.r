9f4c56a172858dd6b24c525bd7ddbefefeaabb7e
FRANCK BENICHOU <franck.benichou@sciencespo.fr>
2021-01-23 14:57:52 -0500

    added new initial R script for EDA, load R script and data frame tranfromation from time series

---
+++ EDA/eda.r
@@ -0,0 +1,26 @@
+# EDA for Part 1 of term project
+#
+#
+#
+#
+
+#install timeseries package
+
+library(timeSeries)
+library(timeDate)
+
+# load the R data
+load('C:/Users/franc/Documents/HEC_MONTREAL/COURSES/2020/winter/forecastingMethods/termProject/sppdata.rdata')
+
+head(sppdata)
+
+sppData = data.frame(Y=as.matrix(sppdata), date=time(sppdata))
+
+
+# export as a csv
+
+write.csv(sppData,'C:/Users/franc/Documents/HEC_MONTREAL/COURSES/2020/winter/forecastingMethods/termProject/sppdata.rdatasppData.csv', row.names = FALSE)
+
+
+sum(is.na(sppdata$WFEC))
+
